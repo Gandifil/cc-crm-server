b9cbf073e0512f2d0754fcd64c33d63062cd1777
max <max@example.com>
2019-04-24 11:00:05 +0400

    added db clearing

--- backend/src/main/java/sgu/csit/backend/init/DbInitializer.java
+++ backend/src/main/java/sgu/csit/backend/init/DbInitializer.java
@@ -7,6 +7,7 @@ import sgu.csit.backend.model.Authority;
 import sgu.csit.backend.model.AuthorityType;
 import sgu.csit.backend.model.User;
 import sgu.csit.backend.repository.AuthorityRepository;
+import sgu.csit.backend.repository.MetersDataRepository;
 import sgu.csit.backend.repository.UserRepository;
 
 import java.util.Collections;
@@ -15,27 +16,40 @@ import java.util.Collections;
 @ConditionalOnProperty(name = "app.db-init", havingValue = "true")
 public class DbInitializer implements CommandLineRunner {
     private AuthorityRepository authorityRepository;
+    private UserRepository userRepository;
+    private MetersDataRepository metersDataRepository;
 
-    public DbInitializer(AuthorityRepository authorityRepository, UserRepository userRepository) {
+    public DbInitializer(AuthorityRepository authorityRepository,
+                         UserRepository userRepository,
+                         MetersDataRepository metersDataRepository) {
         this.authorityRepository = authorityRepository;
+        this.userRepository = userRepository;
+        this.metersDataRepository = metersDataRepository;
     }
 
-    private void initializeAuthorities() {
+    private void clearDB() {
+        System.out.println("Clearing DB...");
+        metersDataRepository.deleteAll();
+        metersDataRepository.flush();
+        userRepository.deleteAll();
+        userRepository.flush();
         authorityRepository.deleteAll();
-
+        authorityRepository.flush();
+        System.out.println("Done!");
+    }
+    private void initializeAuthorities() {
+        System.out.println("Authorities initialization...");
         Authority adminRole = new Authority(AuthorityType.ROLE_ADMIN);
         Authority userRole = new Authority(AuthorityType.ROLE_USER);
 
         authorityRepository.save(adminRole);
         authorityRepository.save(userRole);
+        System.out.println("Done!");
     }
 
     @Override
     public void run(String... args) {
-        System.out.println("DB initialization...");
-
+        clearDB();
         initializeAuthorities();
-
-        System.out.println("Done!");
     }
 }
