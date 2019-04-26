126949268a186a79dc08e16ea44e87719e9339e5
Gandifil <Gandifilmk@gmail.com>
2019-04-26 13:43:40 +0400

    remove test

--- backend/src/test/java/sgu/csit/backend/ApplicationTest.java
+++ backend/src/test/java/sgu/csit/backend/ApplicationTest.java
@@ -12,36 +12,38 @@ import sgu.csit.backend.repository.AuthorityRepository;
 import sgu.csit.backend.repository.UserRepository;
 import sgu.csit.backend.service.UserService;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-//@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ApplicationTest {
-
-    @Autowired
-    private UserRepository repository;
-
-    @Autowired
-    private UserService userService;
-
-    @DisplayName("RegistatorTest")
-    @Test
-    public void registerTest() {
-        System.out.println("RegistatorTest has been started!");
-        assertEquals(repository.existsByUsername("myTestUser"), false);
-
-        User user = new User();
-        user.setUsername("myTestUser");
-        user.setPassword("12341234");
-        user.setFirstName("Максим");
-        user.setSurname("Максимов");
-        user.setMiddleName("Максимович");
-        user.setApartment(1);
-        user.setEmail("fgfdfgd@gmail.com");
-        user.setPhoneNumber("8999999999999");
-
-        userService.register(user);
-        assertEquals(repository.existsByUsername("myTestUser"), true);
-    }
-}
+//
+//import javax.annotation.security.RunAs;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+////@RunAs(SpringRunner.class)
+//@SpringBootTest
+//public class ApplicationTest {
+//
+//    @Autowired
+//    private UserRepository repository;
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @DisplayName("RegistatorTest")
+//    @Test
+//    public void registerTest() {
+//        System.out.println("RegistatorTest has been started!");
+//        assertEquals(repository.existsByUsername("myTestUser"), false);
+//
+//        User user = new User();
+//        user.setUsername("myTestUser");
+//        user.setPassword("12341234");
+//        user.setFirstName("Максим");
+//        user.setSurname("Максимов");
+//        user.setMiddleName("Максимович");
+//        user.setApartment(1);
+//        user.setEmail("fgfdfgd@gmail.com");
+//        user.setPhoneNumber("8999999999999");
+//
+//        userService.register(user);
+//        assertEquals(repository.existsByUsername("myTestUser"), true);
+//    }
+//}
 
